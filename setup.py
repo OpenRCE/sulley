e4beff6077ea1bea3796136a5cbcb1d3629c5299
hdantas <hndantas@gmail.com>
2016-10-17 23:09:07 +0100

    Added setup.py to be able to install sulley more easily.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='sulley',
+    version=0.1,
+    description='A pure-python fully automated'
+                'and unattended fuzzing framework.',
+    author='OpenRCE',
+    packages=['sulley', 'sulley.legos', 'sulley.pgraph', 'sulley.utils'],
+    install_requires=[
+        'pydot'
+    ]
+)
